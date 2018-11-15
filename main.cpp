d884c28a563fb5387b1de3f5af3aa7114ffc4d6c
Eduardo Ávila Vilar <44625476+dudu-av@users.noreply.github.com>
2018-11-15 20:13:52 -0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <tuple>
+#include <vector>
+
 #include "lixo.h"
 #include "lugar.h"
 #include "usuario.h"
 
 int main(){
 
+	// Mapa geral de busca
+	std::map<std::string, std::tuple< std::vector<Usuario>, std::vector<Usuario>, Lixo> mapa_geral;
+	  
+  
   return 0;
 }
