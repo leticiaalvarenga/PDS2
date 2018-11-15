286a8a52c757118f697b828a4ffac47cdf7b505d
leticiaalvarenga <44625475+leticiaalvarenga@users.noreply.github.com>
2018-11-15 19:17:43 -0200

    Update Lixo.cpp

--- Lixo.cpp
+++ Lixo.cpp
@@ -1,15 +1,15 @@
 #include "lixo.h"
 
-Lixo::Lixo(string nome, bool is_toxico, bool is_perecivel, bool is_cortante, bool is_solido, bool is_liquido, bool is_metal, bool is_plastico, bool is_organico){
+Lixo::Lixo(string nome){
     this->_nome = nome;
-    this->_is_toxico = is_toxico;
-    this->_is_perecivel = is_perecivel;
-    this->_is_cortante = is_cortante;
-    this->_is_solido = is_solido;
-    this->_is_liquido = is_liquido;
-    this->_is_metal = is_metal;
-    this->_is_plastico = is_plastico;
-    this->_is_organico = is_organico;
+    this->_is_toxico = false;
+    this->_is_perecivel = false;
+    this->_is_cortante = false;
+    this->_is_solido = false;
+    this->_is_liquido = false;
+    this->_is_metal = false;
+    this->_is_plastico = false;
+    this->_is_organico = false;
 }
 
 string Lixo::Atualizar_lixo(bool is_toxico, bool is_perecivel, bool is_cortante, bool is_solido, bool is_liquido, bool is_metal, bool is_plastico, bool is_organico){
