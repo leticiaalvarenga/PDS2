e6e90ca2ce99763f06117bb532519a8b85091b20
DanielPicarro <45076524+DanielPicarro@users.noreply.github.com>
2018-11-15 18:09:40 -0200

    Add files via upload

---
+++ user.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+	
+
+class Usuario
+{
+private:
+	std::string _nome;
+	//Tipo de Lixo que o usuáro possui (Tipo Lixo)
+	Lixo _resiudo;
+	//Endereco do usuario (Tipo Lugar)
+	Lugar _endereco;
+public:
+	Usuario(std::string nome, Lixo residuo, Lugar endereco);
+	Void Atualiza_lixo(Lixo lixo);
+	Void Atualiza_endereco(Lugar endereco);
+	Void Atualiza_nome(std::string nome);
+	
+	Usuario* Busca(Lixo lixo);
+}
