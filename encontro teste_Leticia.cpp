1a6ba4dbd69e479c759a5735b809fdb19af8477a
leticiaalvarenga <44625475+leticiaalvarenga@users.noreply.github.com>
2018-12-08 12:11:04 -0200

    Update encontro teste_Leticia.cpp
    Acho que tá certo.. Falta testar.

--- encontro teste_Leticia.cpp
+++ encontro teste_Leticia.cpp
@@ -4,11 +4,12 @@
 #include <string>
 #include <iostream>
 #include <map>
+#include <vector>
 
 
 
 
-Encontro::Encontro(std::string data, Endereco endereco, Usuario requisitado, Usuario requisitante, std::bool deu_certo, _confirmado, int id_usuario_encontro)
+Encontro::Encontro(std::string data, Endereco endereco, Usuario requisitado, Usuario requisitante, bool deu_certo, bool confirmado, int id_usuario_encontro)
 {   //ctor
     _data = data;
     _endereco = endereco;
@@ -19,7 +20,7 @@ Encontro::Encontro(std::string data, Endereco endereco, Usuario requisitado, Usu
     _id_usuario_encontro = id_usuario_encontro;
 }
 
-Encontro Encontro::Busca_Encontro(int id_usuario, tipo_mapa_encontros mapa_encontro){
+void Encontro::Busca_Encontro(int id_usuario, tipo_mapa_encontros mapa_encontro){
     //Essa busca de encontros, passa o id do usuário que está querendo fazer a busca,
     //(necessário para conseguir procurar o usuário e procura a lista de vetores de
     //inteiros que tem todos os encontros daquela pessoa e compara os
@@ -27,15 +28,68 @@ Encontro Encontro::Busca_Encontro(int id_usuario, tipo_mapa_encontros mapa_encon
 
     Usuario usuario;
     Usuario usuario_que_procura = usuario.Busca(id_usuario); //Essa busca poderá ser feita com o id
+    std::string aux_encontro;
+    std::string aux_cancelar;
+    std::string aux_confirmar;
 
+    //for para imprimir a lista de encontros
+    int indice = 0;
     for (std::vector<int>::iterator it = usuario_que_procura._encontros.begin(); it != usuario_que_procura._encontros.end(); ++it){
         map::const_iterator pos = mapa_encontro.find(*it);
         if(pos != mapa_encontro.end()){
-            Encontro value = pos->second; //Pega o valor encontro
-            value._requisitado.
+            Encontro valor = pos->second; //Pega o valor encontro
+            std::cout << indice << " - ";
+            std::cout << "Nome do lixo: " << valor._requisitante._lixo <<
+                         ". Data do encontro: " << valor.get_data() <<
+                         ". Endereco do encontro " << valor.get_endereco() << std::endl;
+
+            std::cout << "Deseja escolher este encontro? sim [s], nao [n]" << std::endl;
+            std::cin >> aux_encontro;
+            //Colocar aqui um try catch -> se não for nem s e nem n..
+            if(aux_encontro == "s"){
+                if((usuario_que_procura._id == valor._requisitante._id) && (valor.get_confirmado() == true)){ //usuário que procura é o requisitante
+                    if(valor.get_deu_certo() == true){
+                        ~valor();
+                    }
+                    else{
+                        std::cout << "Cancelar encontro? [s]/ [n]" << std::endl;
+                        std::cin >> aux_cancelar;
+
+                        if(aux_cancelar == "s"){
+                            ~valor();
+                        }
+                        else{
+                            aux_encontro = "n";
+                            break;
+                        }
+                    }
+                }
+
+
+
+                if((usuario_que_procura._id != valor._requisitante._id) && (valor.get_confirmado() == false)){//usuário que procura não é o requisitante
+                    std::cout << "Deseja confirmar o encontro? [s]/ [n] ";
+                    std::cin >> aux_confirmar;
+
+                    if(aux_confirmar == "s"){
+                        valor._confirmado = true;
+                    }
+                    else{
+                        ~valor();
+                    }
+
+                }
+            }
+            else{
+                std::cout << "Próximo da lista.. " << std::endl;
+                indice++;
+            }
+
         }
     }
-    //ctor
+
+
+
 }
 
 Encontro::~Encontro()
