f5201b3e6f66586c38e468db8e630ab2f08386d9
DanielPicarro <45076524+DanielPicarro@users.noreply.github.com>
2018-12-09 18:16:28 -0200

    Update Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -21,9 +21,14 @@ public:
     static unsigned int get_new_id();
 
 	Usuario(tipo_mapa_usuario mapa_usuario, tipo_mapa_geral mapa_geral);
-	//Void Atualiza();
-	//Usuario* Busca();
+	void Atualiza_infos(std::map<std::string, std::vector<std::vector<int> > > mapa);
+	void Atualiza_lixo(std::map<std::string, std::vector<std::vector<int> > > mapa);
+	void Busca(std::map<std::string, std::vector<std::vector<int> > > mapa_geral, std::map<int, Usuario> mapa_usuario); //add
 	void mensagem_de_armazenamento();
+	std::string get_lixo(); //add
+	bool get_if_doador(); //add
+	unsigned int get_id(); //add
+	std::string get_nome(); //add
 };
 
 typedef std::map<int, Usuario> tipo_mapa_usuario;
