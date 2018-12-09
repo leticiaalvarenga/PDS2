4f61a770f9121dc88c28d3b29683fbc9ee1c54ec
leticiaalvarenga <44625475+leticiaalvarenga@users.noreply.github.com>
2018-12-09 21:12:24 -0200

    gets + construtor

--- Encontro.h
+++ Encontro.h
@@ -5,12 +5,30 @@
 class Encontro
 {
     public:
-        Encontro();
+        Encontro(std::string data, Endereco endereco, Usuario requisitado, Usuario requisitante, bool deu_certo, bool _confirmado, int id_usuario_encontro);
         virtual ~Encontro();
+
+//        void Busca_Encontro(int id_usuario, tipo_mapa_encontros mapa_encontro);
+
+        std::string get_data();
+        Endereco get_endereco();
+        Usuario get_requisitante();
+        Usuario get_requisitado();
+        bool get_deu_certo();
+        bool get_confirmado();
+
+
 
     protected:
 
     private:
+        std::string _data;
+        Endereco _endereco;
+        Usuario _requisitado;
+        Usuario _requisitante;
+        bool _deu_certo;
+        bool _confirmado;
+        int _id_usuario_encontro;
 };
 
 #endif // ENCONTRO_H
